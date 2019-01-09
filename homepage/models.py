882a8d5eb3cdf8e011758ea1caa461dc1bc19e6d
masozee <nurojilukmansyah@gmail.com>
2019-01-09 08:53:56 +0700

    perbaikan bug

--- homepage/models.py
+++ homepage/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+class HomeSlide(models.Model):
+    slide1 = models.FileField(upload_to='homepage/')
+    caption1 = models.CharField(max_length=25, null=True, blank=True)
+    subcaption1 = models.CharField(max_length=50, null=True, blank=True)
+    slide2 = models.FileField(upload_to='homepage/')
+    caption2 = models.CharField(max_length=25, null=True, blank=True)
+    subcaption2 = models.CharField(max_length=50, null=True, blank=True)
+    slide3 = models.FileField(upload_to='homepage/')
+    caption3 = models.CharField(max_length=25, null=True, blank=True)
+    subcaption3 = models.CharField(max_length=50, null=True, blank=True)
+
+
--- homepage/views.py
+++ homepage/views.py
@@ -2,12 +2,20 @@ from django.shortcuts import render
 
 
 from singgasanaseni.models import berita
+from homepage.models import HomeSlide
 
 # Create your views here.
 def index(request):
     Berita = berita.object.all().order_by("Upload_date")[:4]
-    return render (request, "index.html", {'Berita': Berita})
+    Slide = HomeSlide.objects.all()
+
+    context = {
+        "Berita": Berita,
+        "Slide": Slide
+    }
+    return render (request, "index.html", context)
 
 
 def tentangkami(request):
     return render (request, "tentangkami.html")
+
